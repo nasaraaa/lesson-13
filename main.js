f600fc1ce03d637c2ada56006ab98097aac3d41d
nasaraaa <159721509+nasaraaa@users.noreply.github.com>
2024-04-01 23:28:32 +0400

    js

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+
+let firstText = "text"
+console.log(firstText)
+
+let x = 20
+let y = 10
+
+
+let sum = y + x;
+console.log("addition", sum)
+let sub = x - y;
+console.log("subtraction", sub)
+let mul = x * y;
+console.log("multiplication", mul)
+let div = x / y;
+console.log("division",div)
+let rem = x % y;
+console.log("resultRem", rem)
+let pow = x ** y;
+console.log("resultPow", pow)
+
+let productname = "Jacket";
+let productprice = 30;
+let productavailable = true;
+let productcolor = null;
+console.log(productavailable,productcolor,productname,productprice) 
+
+
+let myName = "Tornike";
+let myHobby = "Nothing at this moment";
+let yearOfbirth = 2005;
+let currentYear = 2024;
+let ageNow = (currentYear - yearOfbirth);
+console.log(myName,myHobby,yearOfbirth, currentYear, ageNow);
+
+console.log(`my name is ${myName}`)
+console.log(`my hobby ${myHobby}`)
+console.log(`my age at this moment ${currentYear - yearOfbirth}`)
